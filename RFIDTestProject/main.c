fb408d4322bb6b3edf6da727ec7b56a9015120d1
PatrickFairbanks <pfairban@ualberta.ca>
2015-03-18 20:02:29 -0600

    Detect NFC tag implemented
    I commented it out because of the timer function being stupid

--- RFIDTestProject/main.c
+++ RFIDTestProject/main.c
@@ -7,10 +7,14 @@
 #include <IOConfig.h>
 #include <Structs.h>
 #include <XPD.h>
+#include <Thread.h>
 
 #include "time_funcs.h"
 #include "struct_aliases.h"
 #include "UART.h"
+#include "WirelessThreads.h"
+
+#define ERROR 0
 
 static const GlobalPin btTx =  { GPIO_J, io_PJ, 3 ,  Polar_ActiveHigh }; // J3
 static const GlobalPin btRx = { GPIO_J, io_PJ, 2, Polar_ActiveHigh }; // J2
@@ -24,7 +28,8 @@ static const GlobalPin MODE = { GPIO_D, io_PD2, 0x20, Polar_ActiveHigh }; //D2
 static const GlobalPin RTB = { GPIO_D, io_PD3, 0x30, Polar_ActiveHigh }; //D3
 static const size_t kMsPerCycle = 1; //Figure out how to change this to microseconds
 
-//
+
+// Set the Data in pins High or Low. Just for readability
 void dataHigh()
 {
 	globalPin_write(ON, &DIN);
@@ -34,41 +39,50 @@ void dataLow()
 	globalPin_write(OFF, DIN);
 }
 
-//
+//Transceiver Modes
+//Switch To configuration Mode
+
 void configMode()
 {
 	globalPin_write(OFF, &RTB);
 	globalPin_write(OFF, &MODE);
 }
+//Switch to Receive Mode
 void receiveMode()
 {
 	globalPin_write(ON, &RTB);
 	globalPin_write(ON, &MODE);
 }
-
+//Switch to Transmit Mode
 void transmitMode()
 {
 	globalPin_write(OFF, &RTB);
 	globalPin_write(ON, &MODE);
 
 }
-void tModDelay()
+//Delay before reading after Powering NFC tag. Should be 100 us
+void tDelay()
 {
-	transmitMode;
-	//look up function for wait in XINC2
+	uint16_t startTime = timer_read();
+	unint16_t stopTime = 1; // must be 0.1 milliseconds
+	while (startTime < stopTime){
+		startTime = timer_read();
+	}
 }
+//Wireless transmission Function.
+
 int startConfig()
 {
-
-	// This function sets which pins are input and which pins are output. 
-	// Needs pin numbers for Xinc2 and input/output for transceiver
-
+	//Set input and output pin directions
 	globalPin_set_dir(PinDir_Output, &MODE);
 	globalPin_set_dir(PinDir_Output, &RTB);
 	globalPin_set_dir(PinDir_Output, &DIN);
 	globalPin_set_dir(PinDir_Input, &CK);
 	globalPin_set_dir(PinDir_Input, &DOUT);
 	globalPin_set_dir(PinDir_Input, &DSYNC);
+
+	io_set_config(DEFAULT_IO_CFG, &DIN);
+	io_set_config(DEFAULT_TO_CFG, &DOUT);
 
 	//dataHigh();
 	//configMode();
@@ -81,11 +95,38 @@ int startConfig()
 	//Edit Other THings the same way
 	dataLow();
 }
+
+//Not sure if I can have a function of this type
+//If so, This function will, at first (hopefully for not long), will
+//take a command from the bluetooth device read button and will
+//use it to detect a tag, giving an error if not detected.
+
+//uint16_t nfcDetecion(*detectByte){
+//	
+//	uint16_t tagResponse = 0;
+//
+//	transmitMode();
+//	tDelay();
+//
+//	uart_write_byte(detectByte, &DIN);
+//	tDelay();
+//
+//	receiveMode();
+//	tDelay();
+//
+//	tagResponse = uart_read_byte(&DOUT);
+//
+//	if (tagResponse != 0)
+//	{
+//		return(tagResponse);
+//	}
+//	else{
+//		return(ERROR);
+//	}
+//}
 //
 
-//
 
- 
 static const UART_Pin_Pair btPair = { { GPIO_J, io_PJ, 2, Polar_ActiveHigh },  { GPIO_J, io_PJ, 3 ,  Polar_ActiveHigh }, 0 };
 
 void init()
@@ -102,20 +143,47 @@ int test = 0;
 
 int main(void)
 {
+	//Data Transfer initializations
+
+	uint16_t bluetoothCommand = 0;
+	uint16_t tagResponse = 0;
+
 	init();
 	
-	while(1)
-	{
-		test += 1;
-		if( test > 3)
+		while(1)
 		{
-		uart_write_byte('a', &btPair);
+			test += 1;
+			if( test > 3)
+			{
+			uart_write_byte('a', &btpair);
 		
-		uint16_t in = uart_read_byte(&btPair);
-		xpd_echo_int(test, XPD_Flag_SignedDecimal);
+			uint16_t in = uart_read_byte(&btpair);
+			xpd_echo_int(test, xpd_flag_signeddecimal);
 		
-		test = 0;
+			test = 0;
+			}
 		}
-	}
+	//while (1){
+	//	
+	//	bluetoothCommand = uart_read_byte(&btRx);
+
+	//	if (bluetoothCommand == 'a'){
+
+	//		tagResponse = nfcDetection(&bluetoothCommand);
+
+	//		if (tagResponse != ERROR){
+
+	//			uart_write_byte('Receive Mode Active', &btTx);
+	//		}
+	//		else{
+	//			uart_write_byte('You Fucked Up Bad', &btTx);
+	//		}
+
+	//	}
+	//	else{
+	//		uart_write_byte('You Fucked Up Bad', &btTx);
+	//	}
+
+	//}
 	return 0;
 }
