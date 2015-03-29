d33d919fc08e4bd1f7d11129674de348fbb9f349
andyrewsu <and-y-rew@hotmail.com>
2015-03-29 15:17:41 -0600

    MLX.h header file for pin struct

---
+++ RFIDTestProject/inc/MLX.h
@@ -0,0 +1,23 @@
+//--------------------------------------------------------------------------------------
+// File: MLX.h
+// Authors: Andrew Coulthard
+//
+// Copyright (c) University of Alberta. All rights reserved.
+//--------------------------------------------------------------------------------------
+
+#include <IOConfig.h>
+#include <XPD.h>
+
+#include "time_funcs.h"
+
+typedef struct MLX_Pin_Set 
+{
+	GlobalPin const DIN;
+	GlobalPin const DOUT;
+	GlobalPin const DSYNC;
+	GlobalPin const CK;
+	GlobalPin const MODE;
+	GlobalPin const RTB;	
+	
+	static const int periodTicks;
+}
